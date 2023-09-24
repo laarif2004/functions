46c354dac453ef19f75183549dc3b9905864086a
Majd Laarif <96339358+laarif2004@users.noreply.github.com>
2023-09-24 20:27:30 +0100

    Update repetition detection in a table of size a.py

--- repetition detection in a table of size a.py
+++ repetition detection in a table of size a.py
@@ -1,5 +1,5 @@
 import numpy
-def destinct(x,t,n):
+def distinct(x,t,n):
     test=True
     i=0
     while test and i<n:
@@ -12,6 +12,6 @@ a=int(input("Table size: "))
 t=numpy.array([int]*a)
 for i in range(a):
     t[i]=int(input("t["+str(i)+"]="))
-    while destinct(t[i],t,i)==False:
+    while distinct(t[i],t,i)==False:
         t[i]=int(input("you have already written this number,try again please:"))
 print(t)
