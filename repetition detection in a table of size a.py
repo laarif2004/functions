d33a9a0b0e29fbdf06a0547d98ee5c43ad21780d
Majd Laarif <96339358+laarif2004@users.noreply.github.com>
2023-09-24 12:01:51 +0100

    Add files via upload

---
+++ repetition detection in a table of size a.py
@@ -0,0 +1,17 @@
+import numpy
+def destinct(x,t,n):
+    test=True
+    i=0
+    while test and i<n:
+        if t[i]==x:
+            test=False
+        else:
+            i=i+1
+    return(test)
+a=int(input("Table size: "))
+t=numpy.array([int]*a)
+for i in range(a):
+    t[i]=int(input("t["+str(i)+"]="))
+    while destinct(t[i],t,i)==False:
+        t[i]=int(input("you have already written this number,try again please:"))
+print(t)
