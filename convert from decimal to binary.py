46c5a4411c9e116d3df1fcb9b3c44d23a37fe738
Majd Laarif <96339358+laarif2004@users.noreply.github.com>
2023-11-29 09:51:27 +0100

    convert from decimal to binary.py

--- convert from decimal to binary.py
+++ convert from decimal to binary.py
@@ -1,6 +1,9 @@
 def binary(x):
-    a=""
-    while x!=0:
-        a=str(x%2)+a
-        x=x//2
-    return(int(a))
+    if x==0:
+        return(0)
+    else:
+        a=""
+        while x!=0:
+            a=str(x%2)+a
+            x=x//2
+        return(int(a))
